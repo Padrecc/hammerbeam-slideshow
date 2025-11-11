f5a7c5d66fec152852c9def0941cab0a3553a548
Alexey <padrecc@gmail.com>
2025-11-11 13:04:07 +0300

    Update peripheral_status.c

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -87,7 +87,7 @@ const lv_img_dsc_t *anim_imgs[] = {
     &hammerbeam30,
 };
 
-int random_index = lv_rand() % (sizeof(anim_imgs) / sizeof(anim_imgs[0]));
+int random_index = lv_rand(1,30);
 const void *random_image = anim_imgs[random_index];
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
@@ -195,3 +195,4 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 
 
 lv_obj_t *zmk_widget_status_obj(struct zmk_widget_status *widget) { return widget->obj; }
+
