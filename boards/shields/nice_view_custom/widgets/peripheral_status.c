346a625a32a79ca0e6f74510a6f8689d18436a1a
Alexey <padrecc@gmail.com>
2025-11-11 13:27:29 +0300

    Update peripheral_status.c

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -7,7 +7,7 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/random/random.h>
-#include <lv_math.h>
+#include "lv_math.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
@@ -196,5 +196,6 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 
 
 lv_obj_t *zmk_widget_status_obj(struct zmk_widget_status *widget) { return widget->obj; }
+
 
 
