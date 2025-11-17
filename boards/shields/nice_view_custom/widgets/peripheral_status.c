8f93f82e8403347edd026aca2ee270817e0dff21
Alexey <padrecc@gmail.com>
2025-11-17 13:57:18 +0300

    Update peripheral_status.c

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -90,7 +90,7 @@ const lv_img_dsc_t *anim_imgs[] = {
     &hammerbeam30,
 };
 
-void rotateArr(lv_img_dsc_t *arr, int d, int n) {
+void rotateArr(lv_img_dsc_t arr, int d, int n) {
     d %= n;
     lv_img_dsc_t *ranim_imgs[n];
     for (int i = 0; i < n - d; i++)
@@ -208,6 +208,7 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 
 
 lv_obj_t *zmk_widget_status_obj(struct zmk_widget_status *widget) { return widget->obj; }
+
 
 
 
